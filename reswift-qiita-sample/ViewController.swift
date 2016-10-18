517e935bf0e6733d636a152ea299fb65056263ed
rb-de0 <rebirth.de0@gmail.com>
2016-10-18 11:46:05 +0900

    First Commit

---
+++ reswift-qiita-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reswift-qiita-sample
+//
+//  Created by rb_de0 on 2016/10/18.
+//  Copyright © 2016年 rb_de0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
